d75414918851018df2a1abc42fce9c02d713963e
Charlie Hall <charlie.hall@ozemail.com.au>
2018-06-08 01:40:45 +1000

    Implemented babel to satisfy minify.

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-import Dexie from 'dexie';
+'use strict';
 
-let db = null;
-let isSetup = false;
-const storeName = 'pounder-config';
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.generalConfigFallback = undefined;
+exports.getDexie = getDexie;
+exports.initializeDexie = initializeDexie;
 
-export function getDexie() {
+var _dexie = require('dexie');
+
+var _dexie2 = _interopRequireDefault(_dexie);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var db = null;
+var isSetup = false;
+var storeName = 'pounder-config';
+
+function getDexie() {
     if (!isSetup) {
-        throw "Dexie isn't Initialized, ensure you call initializeDexie() first"
-    }
-
-    else {
+        throw "Dexie isn't Initialized, ensure you call initializeDexie() first";
+    } else {
         return db;
     }
 }
 
-export function initializeDexie() {
-    db = new Dexie(storeName);
-    
+function initializeDexie() {
+    db = new _dexie2.default(storeName);
+
     db.version(1).stores({
         cssConfig: "id, propertyName, value",
         generalConfig: "id, value"
-    })
+    });
 
     isSetup = true;
 }
 
 // Fallback Values.
-export const generalConfigFallback = { 
+var generalConfigFallback = exports.generalConfigFallback = {
     startInFullscreen: false,
-    startLocked: false,
-}
+    startLocked: false
+};
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import Dexie from 'dexie';
+
+let db = null;
+let isSetup = false;
+const storeName = 'pounder-config';
+
+export function getDexie() {
+    if (!isSetup) {
+        throw "Dexie isn't Initialized, ensure you call initializeDexie() first"
+    }
+
+    else {
+        return db;
+    }
+}
+
+export function initializeDexie() {
+    db = new Dexie(storeName);
+    
+    db.version(1).stores({
+        cssConfig: "id, propertyName, value",
+        generalConfig: "id, value"
+    })
+
+    isSetup = true;
+}
+
+// Fallback Values.
+export const generalConfigFallback = { 
+    startInFullscreen: false,
+    startLocked: false,
+}
